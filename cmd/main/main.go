d3c93df507fd20bccc544f2e7da8baaaee61a0f8
ridhoperdana <ridhoperdana@ymail.com>
2021-04-30 09:01:21 +0700

    feat: add variable

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"github.com/ridhoperdana/alurgit"
 )
 
 func main()  {
-	alur_git := alurgit.Get_alurgit()
-	fmt.Println(alur_git)
+	var alurGit alurgit.Alurgit
+	alurGit = alurgit.Get_alurgit()
 }
